9c193a8ff19c05b23b196ebd4d1d70c9f5396bed
Mathiman <Mathiman@users.noreply.github.com>
2016-05-25 12:03:27 +0200

    added commments; basic caching(not done); removed gethostbyname_orig

--- dns/resolver.py
+++ dns/resolver.py
@@ -33,7 +33,7 @@ class Resolver(object):
         """ Translate a host name to IPv4 address.
 
         Currently this method contains an example. You will have to replace
-        this example with example with the algorithm described in section
+        this example with the algorithm described in section
         5.3.3 in RFC 1034.
 
         Args:
@@ -62,6 +62,19 @@ class Resolver(object):
         aliases = []
         addresses = []
 
+        #1. See if the answer is in local information, and if so return it to the client.
+        if self.caching:
+            for alias in self.cache.lookup(hostname, Type.CNAME, Class.IN):
+                aliases.append(alias.rdata.data)
+            for address in self.cache.lookup(hostname, Type.A, Class.IN):
+                addresses.append(address.rdata.data)
+
+        if aliases != []:
+            return hostname, aliases, addresses
+
+        #2. Find the best servers to ask.
+
+        #3. Send them queries until one returns a response.
         while not resolved and resolving < parts_len:
             # Create and send query
             question = dns.message.Question(parts[resolving], Type.A, Class.IN)
@@ -73,7 +86,6 @@ class Resolver(object):
 
             for ns in nameservers:
                 sock.sendto(query.to_bytes(), (ns, 53))
-
                 # Receive response
                 data = sock.recv(512)
                 response = dns.message.Message.from_bytes(data)
@@ -97,46 +109,23 @@ class Resolver(object):
                     if answer.type_ == Type.A:
                         addresses.append(answer.rdata.data)
 
-        return hostname, aliases, addresses
 
-    def gethostbyname_orig(self, hostname):
-        """ Translate a host name to IPv4 address.
+   #4. Analyze the response, either:
 
-        Currently this method contains an example. You will have to replace
-        this example with example with the algorithm described in section
-        5.3.3 in RFC 1034.
+    #     a. if the response answers the question or contains a name
+    #        error, cache the data as well as returning it back to
+    #        the client.
 
-        Args:
-            hostname (str): the hostname to resolve
+    #     b. if the response contains a better delegation to other
+    #        servers, cache the delegation information, and go to
+    #        step 2.
 
-        Returns:
-            (str, [str], [str]): (hostname, aliaslist, ipaddrlist)
-        """
-        timeout = 2 # the time waited for a response
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(timeout)
+    #     c. if the response shows a CNAME and that is not the
+    #        answer itself, cache the CNAME, change the SNAME to the
+    #        canonical name in the CNAME RR and go to step 1.
 
-        # Create and send query
-        question = dns.message.Question(hostname, Type.A, Class.IN)
-        header = dns.message.Header(9001, 0, 1, 0, 0, 0)
-        header.qr = 0
-        header.opcode = 0
-        header.rd = 1
-        query = dns.message.Message(header, [question])
-        sock.sendto(query.to_bytes(), ("8.8.8.8", 53))
-
-        # Receive response
-        data = sock.recv(512)
-        response = dns.message.Message.from_bytes(data)
-
-        # Get data
-        aliases = []
-        for additional in response.additionals:
-            if additional.type_ == Type.CNAME:
-                aliases.append(additional.rdata.data)
-        addresses = []
-        for answer in response.answers:
-            if answer.type_ == Type.A:
-                addresses.append(answer.rdata.data)
+    #     d. if the response shows a servers failure or other
+    #        bizarre contents, delete the server from the SLIST and
+    #        go back to step 3.
 
         return hostname, aliases, addresses
