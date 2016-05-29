82618e1a1830638d9d2689d223db2601205588f3
s4436555 <j.moerman@student.ru.nl>
2016-05-29 16:14:56 +0200

    This obviously isn't finished, but the generated messages are borked so I can't test it properly untill that is fixed

--- dns/resolver.py
+++ dns/resolver.py
@@ -29,6 +29,58 @@ class Resolver(object):
         self.caching = caching
         self.ttl = ttl
 
+    def _cache_get_ns_ip(self, hostname):
+        print ("_cache_get_ns_ip")
+        nameservers = []
+        
+        addresses = self.cache.lookup(hostname, Type.A, Class.IN)
+        if addresses != []:
+            for address in p_addresses:
+                nameservers.append(address.rdata.data)
+                    
+        if nameservers != []:
+            return nameservers
+        
+        authoritive_servers = self.cache.lookup(hostname, Type.SOA, Class.IN)
+        for authoritive_server in authoritive_servers:
+            nameservers = _cache_get_ns_ip(authoritive_server.rdata.data)
+            if nameservers != []:
+                break
+        
+        return nameservers
+    
+    def _get_ns_ip(self, nameservers, sock, hostname):
+        print ("_get_ns_ip")
+        new_nameservers = []
+        
+        # Create and send query
+        question = dns.message.Question(hostname, Type.A, Class.IN)
+        header = dns.message.Header(9001, 0, 1, 0, 0, 0)
+        header.qr = 0
+        header.opcode = 0
+        header.rd = 1
+        query = dns.message.Message(header, [question])
+        
+        while new_nameservers == []:
+            for ns in nameservers:
+                sock.sendto(query.to_bytes(), (ns, 53))
+                # Receive response
+                data = sock.recv(512)
+                response = dns.message.Message.from_bytes(data)
+                break #TODO: add timeout code
+            
+            new_nameservers = [ans for ans in response.answers if ans.type_ == Type.A]
+            if new_nameservers == []:
+                authoritive_servers = [ans for ans in response.answers if ans.type_ == Type.SOA]
+                if authoritive_servers != []:
+                    for auth_dns in authoritive_servers:
+                        new_nameservers = _get_ns_ip(nameservers, sock, auth_dns)
+                        if new_nameservers != []:
+                            break
+                else:
+                    break
+            return new_nameservers
+
     def gethostbyname(self, hostname):
         """ Translate a host name to IPv4 address.
 
@@ -42,72 +94,88 @@ class Resolver(object):
         Returns:
             (str, [str], [str]): (hostname, aliaslist, ipaddrlist)
         """
+        
+        
         timeout = 2 # the time waited for a response
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(timeout)
 
-        nameservers = {"8.8.8.8"}
+#        nameservers = {"8.8.8.8"}
+        nameservers = {"localhost"}
         resolved = False
 
         parts = []
         prev = ""
 
         for part in reversed(hostname.split(".")):
-            prev = part + "." + prev
+            if prev != ".":
+                prev = part + "." + prev
+            else:
+                prev = part + prev
             parts.append(prev)
-
-        resolving = 0
+            print (prev)
+        
         parts_len = len(parts)
+        resolving = 0
 
         aliases = []
         addresses = []
 
         #1. See if the answer is in local information, and if so return it to the client.
         if self.caching:
+            resolving = parts_len - 1
             for alias in self.cache.lookup(hostname, Type.CNAME, Class.IN):
                 aliases.append(alias.rdata.data)
             for address in self.cache.lookup(hostname, Type.A, Class.IN):
                 addresses.append(address.rdata.data)
+            
+            if aliases != []:
+                resolved = True
+            else:
+                resolving -= 1
+                while resolving >= 0:
+                    new_nameservers = self._cache_get_ns_ip(parts[resolving])
+                    if new_nameservers != []:
+                        nameservers = new_nameservers
+                        break
+                    resolving -= 1
+                if resolving < 0:
+                    resolving = 0
 
-        if aliases != []:
-            return hostname, aliases, addresses
+        
 
         #2. Find the best servers to ask.
 
         #3. Send them queries until one returns a response.
         while not resolved and resolving < parts_len:
-            # Create and send query
-            question = dns.message.Question(parts[resolving], Type.A, Class.IN)
-            header = dns.message.Header(9001, 0, 1, 0, 0, 0)
-            header.qr = 0
-            header.opcode = 0
-            header.rd = 1
-            query = dns.message.Message(header, [question])
-
-            for ns in nameservers:
-                sock.sendto(query.to_bytes(), (ns, 53))
-                # Receive response
-                data = sock.recv(512)
-                response = dns.message.Message.from_bytes(data)
-                break #TODO: add timeout code
-
             if resolving != parts_len - 1:
-                nameservers = [ans for ans in response.answers if ans.type_ == Type.A]
-                for ans in response.answers:
-                    print ans
-                if (len(nameservers) == 0):
-                    print "no answers"
-                    break
-                print "nameserver: " + nameservers[0]
+                nameservers = self._get_ns_ip(nameservers, sock, parts[resolving])
                 resolving += 1
             else:
-                # Get data
-                for additional in response.additionals:
-                    if additional.type_ == Type.CNAME:
-                        aliases.append(additional.rdata.data)
-                for answer in response.answers:
-                    if answer.type_ == Type.A:
-                        addresses.append(answer.rdata.data)
+                print ("else")
+                # Create and send query
+                question = dns.message.Question(parts[resolving], Type.A, Class.IN)
+                header = dns.message.Header(9001, 0, 1, 0, 0, 0)
+                header.qr = 0
+                header.opcode = 0
+                header.rd = 1
+                query = dns.message.Message(header, [question])
+
+                for ns in nameservers:
+                    sock.sendto(query.to_bytes(), (ns, 53))
+                    # Receive response
+                    data = sock.recv(512)
+                    response = dns.message.Message.from_bytes(data)
+                    break #TODO: add timeout code
+
+                    # Get data
+                    for additional in response.additionals:
+                        if additional.type_ == Type.CNAME:
+                            aliases.append(additional.rdata.data)
+                    for answer in response.answers:
+                        if answer.type_ == Type.A:
+                            addresses.append(answer.rdata.data)
+                resolved = True
 
 
    #4. Analyze the response, either:
